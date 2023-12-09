b4cf69ca41a5b9894ee1915f83ca80cdec2bc8e5
Tarun Malempati <24860718+tarunmalempati@users.noreply.github.com>
2023-12-09 21:32:00 +0530

    Create app.py

---
+++ Docker/machine_learning_app/app.py
@@ -0,0 +1,27 @@
+# app.py
+
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+
+# Load iris dataset
+iris = load_iris()
+X = iris.data
+y = iris.target
+
+# Split the dataset into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Create a RandomForestClassifier model
+model = RandomForestClassifier(n_estimators=100, random_state=42)
+
+# Train the model
+model.fit(X_train, y_train)
+
+# Make predictions on the test set
+predictions = model.predict(X_test)
+
+# Calculate accuracy
+accuracy = accuracy_score(y_test, predictions)
+print(f"Accuracy: {accuracy}")
